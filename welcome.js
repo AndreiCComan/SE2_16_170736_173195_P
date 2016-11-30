edb1fbca392be807c366bf4400b743e2fa40f2ca
cosimodpersia <cosimo.persia@gmail.com>
2016-11-30 09:18:06 +0100

    maps update

--- welcome.js
+++ welcome.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var googleMapsUtil = require('googlemapsutil');
+var polyline = require('polyline-encoded');
 
 //Parse the body in order to use the handlers like req.body
 app.use(bodyParser.urlencoded({
@@ -20,10 +22,48 @@ app.listen(app.get("port"), function () {
 //like images, stylesheets and scripts
 app.use(express.static(__dirname + "/"));
 
+app.post("/getMap", function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+
+    var departure = req.body.departure;
+    var arrival = req.body.arrival;
+    var mode = req.body.mode;
+
+    console.log("-----------------------------------");
+    console.log("Departure received = " + departure);
+    console.log("Arrival received = " + arrival);
+    console.log("Mode received = " + mode);
+    console.log("-----------------------------------");
+
+    departure = departure.replace(/ /g, '+');
+    arrival = arrival.replace(/ /g, '+');
+    mode = mode.replace(/ /g, '+');
+
+    googleMapsUtil.directions(
+        departure,
+        arrival, {
+            mode: mode
+        },
+        function (err, result) {
+            if (err) {
+                console.log(err);
+            } //DA GESTIRE IL CASO IN CUI GOOGLE FALLISCE, server crasha
+            console.log("Response sent back to the request!");
+            result = JSON.parse(result);
+            var polylineDecoded = polyline.decode(result.routes[0].overview_polyline.points);
+            var latlng = [];
+            polylineDecoded.map(function (item) {
+                latlng.push({lat:item[0], lng: item[1]});
+            });
+            result.routes[0].convertedLatLng = latlng;
+            res.send(result);
+        }
+    );
+});
+
 /** Handle POST and GET requests at / path. The response sent back contains the index page where
-  * the user can interact with the app.
-  */
+ * the user can interact with the app.
+ */
 app.use("/", function (req, res) {
     res.end("./index.html");
 });
-
